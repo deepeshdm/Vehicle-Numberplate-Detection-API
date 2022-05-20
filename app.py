fc032ffc3fcb5d32f295f8b0b04a6aa294081a6a
Deepesh Mhatre <deepeshmhatre133@gmail.com>
2022-05-20 23:14:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ app.include_router(vehicle_router, prefix='/detect')
 app.include_router(numberplate_router, prefix='/detect')
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
